ef0a5c016dcb440e1cd2c6ee3ffc5514084564dd
Bastien POINTIS <pointis.bastien@live.fr>
2022-12-13 19:57:25 +0100

    Création d'une vue Tree qui parcourt la base de donnée en triant selon le niveau

--- inventorio/inventaire/models.py
+++ inventorio/inventaire/models.py
@@ -6,8 +6,26 @@ from django.db import models
 class Conteneur(models.Model):
     name = models.fields.CharField(max_length=100)
     parent = models.ForeignKey('self',
-                               default=1,
-                               on_delete=models.SET_DEFAULT)
+                               null=True,
+                               blank=True,
+                               default=None,
+                               on_delete=models.SET_DEFAULT,
+                               related_name='child')
+
+    def set_parent(self, parent):
+        self.parent = parent
+
+    def get_child(self):
+        return self.child.all()
+
+    def get_child_recursively(self):
+        if self.get_child():
+            for child in self.get_child():
+                print(child)
+
 
     def __str__(self):
         return f'{self.name}'
+
+
+
--- inventorio/inventaire/views.py
+++ inventorio/inventaire/views.py
@@ -7,7 +7,13 @@ from inventaire.models import Conteneur
 # Create your views here.
 
 def liste(request):
+    #conteneurs = Conteneur.objects.all()
     conteneurs = Conteneur.objects.all()
+    conteneurs = conteneurs[0]  # root
+
+    print(conteneurs)
+    conteneurs.get_child_recursively()
+
     return render(request,
                   'liste/liste.html',
                   {'conteneurs': conteneurs})
