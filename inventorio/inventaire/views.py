ddb9aef4d670a7479db5f81493b6c99e622fdccb
Bastien POINTIS <pointis.bastien@live.fr>
2022-12-13 12:22:06 +0100

    Création d'une première page et de la base de données

--- inventorio/inventaire/views.py
+++ inventorio/inventaire/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def liste(request):
+    return render(request,
+                  'liste/liste.html')
--- inventorio/inventorio/urls.py
+++ inventorio/inventorio/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from inventaire import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('liste/', views.liste),
+
 ]
